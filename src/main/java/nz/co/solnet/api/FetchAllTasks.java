666d7eaa2cde392ca2e6d6b4b2ae299b3e3f5779
Jackson Tume <42162366+JacksonTume@users.noreply.github.com>
2021-06-10 12:12:09 +1200

    Setup Everything, ready to start writing code

---
+++ src/main/java/nz/co/solnet/api/FetchAllTasks.java
@@ -0,0 +1,19 @@
+package nz.co.solnet.api;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FetchAllTasks extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<H1>"+" HELLO WORLD!!! "+"</H1>");
+    }
+}
